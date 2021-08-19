b71ca158403acd471333837ebb70b35f4e55d2f1
root <565978641@qq.com>
2021-08-19 16:31:28 +0800

    hot fix commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,6 +9,8 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello github!");
         System.out.println("hello github2!");
+        System.out.println("hello github hot fix!");
+
 
     }
 }
