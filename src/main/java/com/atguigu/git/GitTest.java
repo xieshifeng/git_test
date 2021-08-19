26eaff0e4c6e46dcbc3066e3717f4a943a1202b1
root <565978641@qq.com>
2021-08-19 16:15:31 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author XieShifeng
+ * @create 2021-08-19 4:11
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
